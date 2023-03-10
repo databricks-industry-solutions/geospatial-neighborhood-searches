aa3ef6c56ca64b5b2cdf3f4a42b5dbf8e9b868cd
Aaron Zavora <aaron.zavora@databricks.com>
2023-03-10 14:39:16 -0500

    typo

--- src/test/scala/GeoSearch.scala
+++ src/test/scala/GeoSearch.scala
@@ -34,7 +34,7 @@ class GeoSearchTest extends AnyFunSuite{
     //Confirm this is a minimum covering
     assert (
       sw.substring(0, 18) != se.substring(0,18) || sw.substring(0, 18) != nw.substring(0,18) || sw.substring(0, 18) != nes.substring(0,18)
-        || se.substring(0,18) != nw.substring(0,18) || se.substring(0, 18) != news.substring(0,18)
+        || se.substring(0,18) != nw.substring(0,18) || se.substring(0, 18) != nes.substring(0,18)
         || nw.substring(0,18) != nes.substring(0,18)
     )
 
